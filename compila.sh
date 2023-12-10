079e4269243278256e1a40521070adcb708c9f69
Senshiacy <129632321+nathaliayukimi@users.noreply.github.com>
2023-12-10 10:59:42 -0300

    Add files via upload

---
+++ compila.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function Verifica { # Caso a compilação não esteja de acordo, ela se encerra
+	if [ $? -ne 0 ]; then
+		echo "A compilação falhou"
+		exit 1
+	fi
+}
+
+# Compilação da Biblioteca Estática
+gcc -Wall -c -o hashliza.o hashliza.c
+Verifica
+ar -rcv libhashliza.a hashliza.o
+
+# Compilação da Biblioteca Dinâmica
+gcc -c -fPIC -o shannon.o shannon.c -lcurl
+Verifica
+gcc -o libshannon.so -shared shannon.o -lcurl
+Verifica
+
+# Compilação do Executável linkando as bibliotecas
+# Biblioteca Estática
+gcc -Wall -c -o testa.o testa.c
+Verifica
+export LD_LIBRARY_PATH=/path/to/libs:$LD_LIBRARY_PATH
+gcc -Wall -o testa hashliza.o testa.o shannon.o -L${PWD} -lshannon -lm
+Verifica
+
+# Criação do Doxygen
+doxygen -g
+doxygen Doxyfile
+
+# remoção de arquivos fúteis
+rm hashliza.o
+rm shannon.o
+rm testa.o
+
+# Execução do programa de testes.
+./testa
+
+exit 0
